e0b44f342ceb9e46314fd9d2773d1a577ce04bbd
Konstantin <relict.adm@gmail.com>
2025-04-24 21:13:29 +0300

    wip

---
+++ project/libs/shared/core/src/index.ts
@@ -0,0 +1,5 @@
+export { Entity } from './lib/base/entity'
+
+export { UserRoleEnum } from './lib/types/user-role.enum'
+export type { UserInterface } from './lib/types/user.interface'
+export type { AuthUserInterface } from './lib/types/auth-user.interface'
---
+++ project/libs/shared/core/src/lib/types/base.interface.ts
@@ -0,0 +1,5 @@
+export interface BaseInterface {
+  id?: string|number;
+}
+
+
